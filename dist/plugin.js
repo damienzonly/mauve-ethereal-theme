daf4fac8049947b4f1f98e861b4030eb58950bbb
damienzonly <damianomails@gmail.com>
2024-05-01 02:47:04 +0200

    first commit

---
+++ dist/plugin.js
@@ -0,0 +1,5 @@
+exports.version = 1.0
+exports.description = "Light/Dark theme for HFS"
+exports.apiRequired = 1
+exports.repo = "damienzonly/mauve-ethereal-theme"
+exports.frontend_css = 'style.css'
